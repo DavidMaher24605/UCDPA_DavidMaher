da040dd381c06a7b1c2a0214957f9b7db6e28540
david maher <davidmaher24605@gmail.com>
2021-10-24 12:39:13 +0100

    Initial Exploration

--- main.py
+++ main.py
@@ -13,7 +13,18 @@ print(data.head)
 
 
 print(data.columns.values)
+# Too many column headders #
+# to do list #
+# Graph data cases over time#
+# drop irrelevant data smoking status, 'cardiovasc_death_rate', 'cardiovasc_death_rate' 'stringency_index' #
 
+print(data.date)
+pd.set_option('display.max_rows', 500)
+print(data['location'].value_counts())
+
+data.del(columns=['female_smokers'], axis=1)
+
+print(data.columns.values)
 
 
 
