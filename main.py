2bc9b178e4e4fd890715e18bbe126b5808411e0a
david maher <davidmaher24605@gmail.com>
2021-10-29 17:15:31 +0100

    Initial more graphic and sub setting

--- main.py
+++ main.py
@@ -70,13 +70,63 @@ plt.ylabel('New Cases')
 plt.savefig('New Cases' + '.png')
 plt.show()
 
-#ned to find out where cases are int#
+#need to find out where cases are int#
+grouped = data.groupby(data.continent)
+Europe = grouped.get_group("Europe")
+print(Europe)
 
+Europe['date'] = pd.to_datetime(data['date'])
+Europe.sort_values('date', inplace=True)
+Newcase_date = Europe['date']
+Number_of_NewCases = Europe['new_cases']
+plt.plot_date(Newcase_date, Number_of_NewCases, linestyle='solid')
+plt.gcf().autofmt_xdate()
+date_format = mpl_dates.DateFormatter('%d-%m-%Y')
+plt.gca().xaxis.set_major_formatter(date_format)
+plt.tight_layout()
+plt.title('New cases per day')
+plt.xlabel('Date')
+plt.ylabel('New Cases')
+plt.savefig('New Cases' + '.png')
+plt.show()
 
+pos_count, neg_count = 0, 0
 
+for num in Europe['new_cases']:
 
+ # checking condition
+ if num < 0:
+  neg_count += 1
 
+print("Negative numbers in the list: ", neg_count)
 
+print('neg_count')
 
+print()
 
+Europe.new_cases =pd.to_numeric(Europe.new_cases, errors ='coerce').fillna(0).astype(int)
+
+print(Europe[Europe['new_cases']<0])
+
+Europe_NC = Europe.where(Europe['new_cases'] > 0, 0)
+
+print(Europe_NC)
+Europe_NC['date'] = pd.to_datetime(data['date'])
+Europe_NC.sort_values('date', inplace=True)
+Newcase_date = Europe_NC['date']
+Number_of_NewCases = Europe_NC['new_cases']
+plt.plot_date(Newcase_date, Number_of_NewCases, linestyle='solid')
+plt.gcf().autofmt_xdate()
+date_format = mpl_dates.DateFormatter('%d-%m-%Y')
+plt.gca().xaxis.set_major_formatter(date_format)
+plt.tight_layout()
+plt.title('New cases per day')
+plt.xlabel('Date')
+plt.ylabel('New Cases')
+plt.savefig('New Cases' + '.png')
+plt.show()
+
+# need to smooth out the data#
+#need to constrain the dates#
+#machine Leanring Linear regeression#
 
