0c332496a6b6a20a861780eccdd0f02de42526e9
david maher <davidmaher24605@gmail.com>
2021-10-31 19:39:35 +0000

    Cleaning removed blanks in location, need to sort date issue and copy warning.

--- main.py
+++ main.py
@@ -26,8 +26,8 @@ print(data['location'].value_counts())
 
 print(data.shape)
 
-data.drop(['female_smokers','cardiovasc_death_rate', 'cardiovasc_death_rate' ,'stringency_index','diabetes_prevalence','handwashing_facilities','male_smokers','aged_65_older' ,'aged_70_older','excess_mortality_cumulative_absolute', 'excess_mortality_cumulative',
- 'excess_mortality' ,'excess_mortality_cumulative_per_million'], axis='columns', inplace=True)
+data.drop(['female_smokers','cardiovasc_death_rate','cardiovasc_death_rate','stringency_index','diabetes_prevalence','handwashing_facilities','male_smokers','aged_65_older' ,'aged_70_older','excess_mortality_cumulative_absolute', 'excess_mortality_cumulative',
+ 'excess_mortality','excess_mortality_cumulative_per_million'], axis='columns', inplace=True)
 
 print(data.shape)
 print(data['date'].min())
@@ -38,7 +38,12 @@ print(data.loc[1361]['date'])
 print(data.loc[4498]['date'])
 print(data.loc[1]['new_cases'])
 data['new_cases'] = data['new_cases'].fillna(0)
-data['location'] = data['location'].replace(0, "unknown")
+data['location'] = data['location'].fillna(0)
+data['location'].replace('', np.nan, inplace=True)
+data['location'] = data['location'].astype(str)
+data['date'] = data['date'].astype('datetime64[ns]')
+data['date'] = pd.to_datetime(data['date'], format='%y%m%d')
+
 pos_count, neg_count = 0, 0
 
 for num in data['new_cases']:
@@ -73,7 +78,8 @@ plt.show()
 #need to find out where cases are int#
 grouped = data.groupby(data.continent)
 Europe = grouped.get_group("Europe")
-print(Europe)
+Europe_group_sort= Europe.sort_values("location").head()
+print(Europe_group_sort['location'])
 
 Europe['date'] = pd.to_datetime(data['date'])
 Europe.sort_values('date', inplace=True)
@@ -94,15 +100,13 @@ pos_count, neg_count = 0, 0
 
 for num in Europe['new_cases']:
 
- # checking condition
+ # checking condition#
  if num < 0:
   neg_count += 1
 
 print("Negative numbers in the list: ", neg_count)
 
 print('neg_count')
-
-print()
 
 Europe.new_cases =pd.to_numeric(Europe.new_cases, errors ='coerce').fillna(0).astype(int)
 Europe['location'] = Europe['location'].replace("0","unknown")
@@ -114,9 +118,21 @@ Europe_NC = Europe.where(Europe['new_cases'] > 0, 0)
 print(Europe_NC['location'].value_counts())
 
 #finding pattern with Regex#
-Regex_search=Europe_NC['location'].str.contains(r'0')
+Regex_search =Europe_NC['location'].str.contains(r'0')
 
-print(Regex_search)
+#sns.lineplot(data=Europe_NC, x="date", y="new_cases")#
+
+print(Europe_NC.shape)
+
+Europe_NC_EU= Europe_NC.drop(Europe_NC.index[Europe_NC['location'].isin([' ',0,'Andorra','United Kingdom','vatican','San Marino','Liechtenstein','Monaco','Iceland','Kosovo','Bosnia and Herzegovina','Switzerland','Montenegro','Belarus','Russia','Serbia','Ukraine','North Macedonia','Albania','Norway'])])
+date_formatt= mpl_dates.DateFormatter('%d-%m-%Y')
+
+#Europe_NC_2021 = Europe_NC.loc[(Europe_NC['date'] > '01-01-2021')]#
+#Europe_NC['date'] = pd.to_datetime(Europe_NC['date']).dt.date#
+print(Europe_NC_EU.location)
+
+
+
 
 
 
@@ -129,3 +145,21 @@ print(Regex_search)
 #need to constrain the dates#
 #machine Leanring Linear regeression#
 
+
+# prepare the lists for the model
+# X = date_format
+# y = data['gravi_deceduti'].tolist()[1:]
+# # date format is not suitable for modeling, let's transform the date into incrementals number starting from April 1st
+# starting_date = 37  # April 1st is the 37th day of the series
+# day_numbers = []
+# for i in range(1, len(X)):
+#     day_numbers.append([i])
+# X = day_numbers
+# # # let's train our model only with data after the peak
+# X = X[starting_date:]
+# y = y[starting_date:]
+# # Instantiate Linear Regression
+# linear_regr = linear_model.LinearRegression()
+# # Train the model using the training sets
+# linear_regr.fit(X, y)
+# print ("Linear Regression Model Score: %s" % (linear_regr.score(X, y)))#
